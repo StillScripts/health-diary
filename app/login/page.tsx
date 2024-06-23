b03a66539973bac3151430fb4c606939461bc927
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-06-23 21:26:15 +1000

    Move chat pages to (chat)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
 import LoginForm from '@/components/login-form'
 import { getServerUser } from '@/lib/supabase/server'
-import { Session } from '@/lib/types'
 import { redirect } from 'next/navigation'
 
 export default async function LoginPage() {
