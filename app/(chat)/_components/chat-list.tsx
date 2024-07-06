6064ea5770e6c07122a70794b031c70244965841
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-07-06 12:52:15 +1000

    Add `prettier-plugin-tailwindcss`

--- app/(chat)/_components/chat-list.tsx
+++ app/(chat)/_components/chat-list.tsx
@@ -5,48 +5,48 @@ import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
 import type { SupabaseSession } from '@/lib/supabase/server'
 
 export interface ChatList {
-  messages: UIState
-  session?: SupabaseSession
-  isShared: boolean
+	messages: UIState
+	session?: SupabaseSession
+	isShared: boolean
 }
 
 export function ChatList({ messages, session, isShared }: ChatList) {
-  if (!messages.length) {
-    return null
-  }
+	if (!messages.length) {
+		return null
+	}
 
-  return (
-    <div className="relative mx-auto max-w-2xl px-4">
-      {!isShared && !session ? (
-        <>
-          <div className="group relative mb-4 flex items-start md:-ml-12">
-            <div className="bg-background flex size-[25px] shrink-0 select-none items-center justify-center rounded-md border shadow-sm">
-              <ExclamationTriangleIcon />
-            </div>
-            <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
-              <p className="text-muted-foreground leading-normal">
-                Please{' '}
-                <Link href="/login" className="underline">
-                  log in
-                </Link>{' '}
-                or{' '}
-                <Link href="/signup" className="underline">
-                  sign up
-                </Link>{' '}
-                to save and revisit your chat history!
-              </p>
-            </div>
-          </div>
-          <Separator className="my-4" />
-        </>
-      ) : null}
+	return (
+		<div className="relative mx-auto max-w-2xl px-4">
+			{!isShared && !session ? (
+				<>
+					<div className="group relative mb-4 flex items-start md:-ml-12">
+						<div className="flex size-[25px] shrink-0 select-none items-center justify-center rounded-md border bg-background shadow-sm">
+							<ExclamationTriangleIcon />
+						</div>
+						<div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
+							<p className="leading-normal text-muted-foreground">
+								Please{' '}
+								<Link href="/login" className="underline">
+									log in
+								</Link>{' '}
+								or{' '}
+								<Link href="/signup" className="underline">
+									sign up
+								</Link>{' '}
+								to save and revisit your chat history!
+							</p>
+						</div>
+					</div>
+					<Separator className="my-4" />
+				</>
+			) : null}
 
-      {messages.map((message, index) => (
-        <div key={message.id}>
-          {message.display}
-          {index < messages.length - 1 && <Separator className="my-4" />}
-        </div>
-      ))}
-    </div>
-  )
+			{messages.map((message, index) => (
+				<div key={message.id}>
+					{message.display}
+					{index < messages.length - 1 && <Separator className="my-4" />}
+				</div>
+			))}
+		</div>
+	)
 }
