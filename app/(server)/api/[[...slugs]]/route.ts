76d68798cb6549c5dc879a0d8160fbf53529a9b7
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-09-14 15:52:08 +1000

    Setup Elysia database routes

--- app/(server)/api/[[...slugs]]/route.ts
+++ app/(server)/api/[[...slugs]]/route.ts
@@ -2,6 +2,12 @@
 // app/api/[[...slugs]]/route.ts
 import { Elysia, t } from 'elysia'
 
+import { db } from '@/db/connection';
+import { exerciseEvents, exercises } from '@/db/schema';
+
+//https://github.com/itsyoboieltr/dbest-stack/blob/main/src/routes/api/todo/index.ts
+
+
 const app = new Elysia({ prefix: '/api' })
     .get('/', () => 'hello Next')
     .post('/', ({ body }) => body, {
@@ -10,6 +16,8 @@ const app = new Elysia({ prefix: '/api' })
         })
     })
     .get('/demo', ()=>({hello:'world'}))
+    .get('/exercise-events', async () => await db.select().from(exerciseEvents))
+    .get('/exercises', async () => await db.select().from(exercises))
 
 export const GET = app.handle 
 export const POST = app.handle 
