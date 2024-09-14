1267ca2344687f1dc4b780cda8c523ae90af18df
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-09-14 16:02:01 +1000

    Add auth example

--- app/(server)/api/[[...slugs]]/route.ts
+++ app/(server)/api/[[...slugs]]/route.ts
@@ -4,6 +4,7 @@ import { Elysia, t } from 'elysia'
 
 import { db } from '@/db/connection';
 import { exerciseEvents, exercises } from '@/db/schema';
+import { getServerUser } from '@/lib/supabase/server';
 
 //https://github.com/itsyoboieltr/dbest-stack/blob/main/src/routes/api/todo/index.ts
 
@@ -16,7 +17,14 @@ const app = new Elysia({ prefix: '/api' })
         })
     })
     .get('/demo', ()=>({hello:'world'}))
-    .get('/exercise-events', async () => await db.select().from(exerciseEvents))
+    .get('/exercise-events', async () => await db.select().from(exerciseEvents), {
+        // TODO check auth before any other action
+        beforeHandle({ set, cookie: { session } }) {
+            if (!session?.cookie?.domain) {
+                return (set.status = 'Unauthorized')
+            }
+        }
+    })
     .get('/exercises', async () => await db.select().from(exercises))
 
 export const GET = app.handle 
