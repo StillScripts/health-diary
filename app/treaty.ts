4c2136d645525fb81428a81ab41954876b8a1a35
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-09-25 22:14:29 +1000

    fix url

--- app/treaty.ts
+++ app/treaty.ts
@@ -6,6 +6,8 @@ if (process.env.VERCEL_ENV === 'preview') {
 	url = 'https://' + process.env.VERCEL_BRANCH_URL
 } else if (process.env.VERCEL_ENV === 'production') {
 	url = 'https://' + process.env.VERCEL_PROJECT_PRODUCTION_URL
+} else if (process.env.NODE_ENV === 'production') {
+	url = 'https://health-diary-theta.vercel.app'
 }
 
 export const app = treaty<App>(url, {})
